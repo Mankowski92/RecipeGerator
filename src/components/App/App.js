c3e66ee1e5042d8740e8a3ec12a218125bad65a2
Marcin Mańkowski <mankowski92@gmail.com>
2020-02-24 10:56:56 +0100

    innitial commit

---
+++ src/components/App/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+// import logo from './logo.svg';
+import './App.scss';
+
+function App() {
+  return (
+        <p>
+          This is the very beginning of RecipeGerator App
+        </p>
+  );
+}
+
+export default App;
