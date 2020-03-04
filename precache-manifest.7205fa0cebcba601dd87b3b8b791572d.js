5866492a6fa61d542687d81aacdb93fa2917e66a
Marcin Mańkowski <mankowski92@gmail.com>
2020-03-04 16:27:16 +0100

    Updates

---
+++ precache-manifest.7205fa0cebcba601dd87b3b8b791572d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ee0aea40a0f8954878a4a9c2d4cd7e7",
+    "url": "/RecipeGerator/index.html"
+  },
+  {
+    "revision": "3b19753802c2f0fd2f32",
+    "url": "/RecipeGerator/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "9b741b1d6ad2e17fb9e9",
+    "url": "/RecipeGerator/static/css/main.3ba8e89a.chunk.css"
+  },
+  {
+    "revision": "3b19753802c2f0fd2f32",
+    "url": "/RecipeGerator/static/js/2.11c8c916.chunk.js"
+  },
+  {
+    "revision": "299cba5f6c747cbc295097594263041b",
+    "url": "/RecipeGerator/static/js/2.11c8c916.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b741b1d6ad2e17fb9e9",
+    "url": "/RecipeGerator/static/js/main.11c6b541.chunk.js"
+  },
+  {
+    "revision": "b4d7d78c2acdee9a9ad7",
+    "url": "/RecipeGerator/static/js/runtime-main.4623238f.js"
+  },
+  {
+    "revision": "e398cba041bcf48317865f76d9106879",
+    "url": "/RecipeGerator/static/media/howDareYou.e398cba0.jpg"
+  },
+  {
+    "revision": "5d22279056e1d63e6dec2f838c6ad045",
+    "url": "/RecipeGerator/static/media/main-background-alt.5d222790.jpeg"
+  }
+]);
