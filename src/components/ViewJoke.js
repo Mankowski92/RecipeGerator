199ff10b12f72e09aed44fe0e521a0801db3e822
Marcin Mańkowski <mankowski92@gmail.com>
2020-03-21 17:50:16 +0100

    viewjoke component reformated

--- src/components/ViewJoke.js
+++ src/components/ViewJoke.js
@@ -1,41 +1,40 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import "../scss/Joke.scss";
 
 const ViewJoke = () => {
-    const [data, setData] = useState(false);
+  const [data, setData] = useState(false);
 
-    // const ingredientsList = ['bananas', 'apples', 'cheese', 'crackers'];
+  // const ingredientsList = ['bananas', 'apples', 'cheese', 'crackers'];
 
-    useEffect(() => {
-        fetch("https://api.spoonacular.com/food/jokes/random?apiKey=72537751dcd74f0cbe54623e5064fa3b", 
-        {
+  useEffect(() => {
+    fetch(
+      "https://api.spoonacular.com/food/jokes/random?apiKey=72537751dcd74f0cbe54623e5064fa3b",
+      {
         method: "GET",
-        headers: {  
-        "Content-Type": "application/json"
-          }
-        }).then(response => {
-            if (response.ok) {
-                return response.json();
-            } else {
-                throw new Error("net error");
-            }
-        })
-        .then(data => {
-            setData(data);
-            })
-        .catch(err => {
-            console.error("Error appeard - " + err.message)
-        })
-    },[])
+        headers: {
+          "Content-Type": "application/json"
+        }
+      }
+    )
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error("net error");
+        }
+      })
+      .then(data => {
+        setData(data);
+      })
+      .catch(err => {
+        console.error("Error appeard - " + err.message);
+      });
+  }, []);
 
-    if (data ===false ) {
-        return <h1>Ładuję...</h1>
-    }
-    return (
-        <div className="joke-div">
-            {data.text}
-        </div>
-      );
-    }
+  if (data === false) {
+    return <h1>Ładuję...</h1>;
+  }
+  return <div className="joke-div">{data.text}</div>;
+};
 
-export default ViewJoke
+export default ViewJoke;
