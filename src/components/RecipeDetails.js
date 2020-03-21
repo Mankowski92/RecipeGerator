e862bb7ec160c3e36253674595a80f9b3726a392
Marcin Mańkowski <mankowski92@gmail.com>
2020-03-21 17:49:43 +0100

    Recipe search and details reforamted

--- src/components/RecipeDetails.js
+++ src/components/RecipeDetails.js
@@ -1,50 +1,56 @@
-import React, {useState, useEffect} from "react";
-import "../scss/RecipeDetails.scss"
+import React, { useState, useEffect } from "react";
+import "../scss/RecipeDetails.scss";
 
-const RecipeDetails = ({recipeInfo}) => {
-    const [data, setData] = useState(false);
+const RecipeDetails = ({ recipeInfo }) => {
+  const [data, setData] = useState(false);
 
-    useEffect(() => {
-        fetch(`https://api.spoonacular.com/recipes/${recipeInfo}/information?apiKey=72537751dcd74f0cbe54623e5064fa3b`, 
-        {
+  useEffect(() => {
+    fetch(
+      `https://api.spoonacular.com/recipes/${recipeInfo}/information?apiKey=72537751dcd74f0cbe54623e5064fa3b`,
+      {
         method: "GET",
-        headers: {  
-        "Content-Type": "application/json"
-          }
-        }).then(response => {
-            if (response.ok) {
-                return response.json();
-            } else {
-                throw new Error("Some hungry error apeard");
-            }
-        })
-        .then(data => {
-            setData(data);
-            console.log(data)
-            })
-        .catch(err => {
-            console.error("Unfortunately, there was a hungry mistake: " + err.message)
-        })
-    },[recipeInfo])
+        headers: {
+          "Content-Type": "application/json"
+        }
+      }
+    )
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error("Some hungry error apeard");
+        }
+      })
+      .then(data => {
+        setData(data);
+        console.log(data);
+      })
+      .catch(err => {
+        console.error(
+          "Unfortunately, there was a hungry mistake: " + err.message
+        );
+      });
+  }, [recipeInfo]);
 
-    if (data ===false ) {
-        return <h1>Brace yourself recipe is coming...</h1>
-    }
-    return (
-        <div className= "detail-container">
-            <div className= "title-img-container">
-            <p>
-                Dish health Score:{data.healthScore}
-            </p>    
-            <img className= "recipeDetail-image" src={data.image} alt="Tekst alternatywny"></img>
-            </div>
-            <div className= "summary-container">
-                <span dangerouslySetInnerHTML={{__html: data.summary}} />
-            </div>
-            <div className= "summary-container">
-            </div>
-        </div>
-      );
-    }
+  if (data === false) {
+    return <h1>Brace yourself recipe is coming...</h1>;
+  }
+  return (
+    <div className="detail-container">
+      <div className="title-img-container">
+        <p>Dish health Score:{data.healthScore}</p>
+        <img
+          className="recipeDetail-image"
+          src={data.image}
+          alt="Tekst alternatywny"
+        ></img>
+      </div>
+      <div className="summary-container">
+        <span dangerouslySetInnerHTML={{ __html: data.summary }} />
+      </div>
+      <div className="summary-container"></div>
+    </div>
+  );
+};
 
-export default RecipeDetails
+export default RecipeDetails;
--- src/components/SearchRecipe.js
+++ src/components/SearchRecipe.js
@@ -1,143 +1,168 @@
-import React, {useState} from "react";
-import "../scss/SearchRecipe.scss"
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
-import Col from 'react-bootstrap/Col';
+import React, { useState } from "react";
+import "../scss/SearchRecipe.scss";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import Col from "react-bootstrap/Col";
 
+const SearchRecipe = props => {
+  const [data] = useState(null);
+  const [query, setQuery] = useState("");
+  const [cuisine, setCuisine] = useState("");
+  const [diet, setDiet] = useState("");
+  const [excludeIngredients, setexcIudeIngredients] = useState("");
+  const [intolerances, setIntolerances] = useState("");
+  const [number, setNumber] = useState(2);
 
-const SearchRecipe = (props) => {
-    const [data] = useState(null);
-    const [query, setQuery] = useState("");
-    const [cuisine, setCuisine] = useState("");
-    const [diet, setDiet] = useState("");
-    const [excludeIngredients, setexcIudeIngredients] = useState("");
-    const [intolerances, setIntolerances] = useState("");
-    const [number, setNumber] = useState(2);
+  const handleSubmit = e => {
+    e.preventDefault();
 
-    
-    const handleSubmit = (e) => {
-        e.preventDefault();
-
-        fetch(`https://api.spoonacular.com/recipes/search?query=${query}&cuisine=${cuisine}&diet=${diet}&excludeIngredients=${excludeIngredients}&intolerances=${intolerances}&number=${number}&apiKey=72537751dcd74f0cbe54623e5064fa3b`, 
-        {
+    fetch(
+      `https://api.spoonacular.com/recipes/search?query=${query}&cuisine=${cuisine}&diet=${diet}&excludeIngredients=${excludeIngredients}&intolerances=${intolerances}&number=${number}&apiKey=72537751dcd74f0cbe54623e5064fa3b`,
+      {
         method: "GET",
-        headers: {  
-        "Content-Type": "application/json"
-          }
-        }).then(response => {
-            if (response.ok) {
-                return response.json();
-            } else {
-                throw new Error("Some hungry error apeard");
-            }
-        })
-        .then(data => {
-            props.onFound(data.results);
-            // console.log(data)
-        })
-            
-        .catch(err => {
-            console.error("Unfortunately, there was a hungry mistake: " + err.message)
-        })
-    }
-    
-    if (data ===false ) {
-        return <h1>Searching...</h1>
+        headers: {
+          "Content-Type": "application/json"
         }
-    
-    return (
+      }
+    )
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error("Some hungry error apeard");
+        }
+      })
+      .then(data => {
+        props.onFound(data.results);
+        // console.log(data)
+      })
 
-<Form>
-  <Form.Row>
-    <Form.Group as={Col} controlId="formGridText">
-      <Form.Label>Dish type</Form.Label>
-      <Form.Control value={query} onChange={e => setQuery(e.target.value)} type="text" placeholder="e.g Hamburger" />
-    </Form.Group>
-    <Form.Group as={Col} controlId="formGridState">
-      <Form.Label>Cousine type</Form.Label>
-      <Form.Control value={cuisine} onChange={e => setCuisine(e.target.value)} as="select">
-        <option value="">All</option>
-        <option value="african">African</option>
-        <option value="american">American</option>
-        <option value="british">British</option>
-        <option value="cajun">Cajun</option>
-        <option value="caribbean">Caribbean</option>
-        <option value="chinese">Chinese</option>
-        <option value="eastern european">Eastern European</option>
-        <option value="european">European</option>
-        <option value="french">French</option>
-        <option value="german">German</option>
-        <option value="greek">Greek</option>
-        <option value="indian">Indian</option>
-        <option value="irish">Irish</option>
-        <option value="italian">Italian</option>
-        <option value="japanese">Japanese</option>
-        <option value="jewish">Jewish</option>
-        <option value="korean">Korean</option>
-        <option value="latin american">Latin American</option>
-        <option value="mediterranean">Mediterranean</option>
-        <option value="mexican">Mexican</option>
-        <option value="eiddle eastern">Middle Eastern</option>
-        <option value="nordic">Nordic</option>
-        <option value="southern">Southern</option>
-        <option value="spanish">Spanish</option>
-        <option value="thai">Thai</option>
-        <option value="vietnamese">Vietnamese</option>
-      </Form.Control>
-    </Form.Group>
-  </Form.Row>
-  <Form.Row>
-    <Form.Group as={Col} controlId="formGridText">
-      <Form.Label>Excluded ingredients</Form.Label>
-      <Form.Control value={excludeIngredients} onChange={e => setexcIudeIngredients(e.target.value)} type="text" placeholder="e.g Pasta" />
-    </Form.Group>
-    <Form.Group as={Col} controlId="formGridState">
-      <Form.Label>Diet type</Form.Label>
-      <Form.Control value={diet} onChange={e => setDiet(e.target.value)} as="select">
-        <option value="">All</option>
-        <option value="gluten free">Gluten Free</option>
-        <option value="vegetarian">Vegetarian</option>
-        <option value="lacto-vegetarian">Lacto-Vegetarian</option>
-        <option value="ovo-vegetarian">Ovo-Vegetarian</option>
-        <option value="vegan">Vegan</option>
-        <option value="pescetarian">Pescetarian</option>
-        <option value="paleo">Paleo</option>
-        <option value="primal">Primal</option>
-        <option value="whole30">Whole30</option>
-      </Form.Control>
-    </Form.Group>
-  </Form.Row>
-  <Form.Row>
-    <Form.Group as={Col} controlId="formGridText">
-      <Form.Label>How many recipes for you?</Form.Label>
-      <Form.Control value={number} onChange={e => setNumber(e.target.value)} type="number" placeholder="e.g 3- (up to 100)" />
-    </Form.Group>
-    <Form.Group as={Col} controlId="formGridState">
-      <Form.Label>Intolerances</Form.Label>
-      <Form.Control value={intolerances} onChange={e => setIntolerances(e.target.value)} as="select">
-        <option value="">None</option>
-        <option value="dairy">Dairy</option>
-        <option value="egg">Egg</option>
-        <option value="gluten">Gluten</option>
-        <option value="grain">Grain</option>
-        <option value="peanut">Peanut</option>
-        <option value="seafood">Seafood</option>
-        <option value="sesame">Sesame</option>
-        <option value="shellfish">Shellfish</option>
-        <option value="soy">Soy</option>
-        <option value="sulfite">Sulfite</option>
-        <option value="tree nut">Tree aNut</option>
-        <option value="wheat">Wheat</option>
-      </Form.Control>
-    </Form.Group>
-  </Form.Row>
-  <Button onClick={handleSubmit}>
-    Submit
-  </Button>
-</Form>
+      .catch(err => {
+        console.error(
+          "Unfortunately, there was a hungry mistake: " + err.message
+        );
+      });
+  };
 
-      );
-    }
+  if (data === false) {
+    return <h1>Searching...</h1>;
+  }
 
+  return (
+    <Form>
+      <Form.Row>
+        <Form.Group as={Col} controlId="formGridText">
+          <Form.Label>Dish type</Form.Label>
+          <Form.Control
+            value={query}
+            onChange={e => setQuery(e.target.value)}
+            type="text"
+            placeholder="e.g Hamburger"
+          />
+        </Form.Group>
+        <Form.Group as={Col} controlId="formGridState">
+          <Form.Label>Cousine type</Form.Label>
+          <Form.Control
+            value={cuisine}
+            onChange={e => setCuisine(e.target.value)}
+            as="select"
+          >
+            <option value="">All</option>
+            <option value="african">African</option>
+            <option value="american">American</option>
+            <option value="british">British</option>
+            <option value="cajun">Cajun</option>
+            <option value="caribbean">Caribbean</option>
+            <option value="chinese">Chinese</option>
+            <option value="eastern european">Eastern European</option>
+            <option value="european">European</option>
+            <option value="french">French</option>
+            <option value="german">German</option>
+            <option value="greek">Greek</option>
+            <option value="indian">Indian</option>
+            <option value="irish">Irish</option>
+            <option value="italian">Italian</option>
+            <option value="japanese">Japanese</option>
+            <option value="jewish">Jewish</option>
+            <option value="korean">Korean</option>
+            <option value="latin american">Latin American</option>
+            <option value="mediterranean">Mediterranean</option>
+            <option value="mexican">Mexican</option>
+            <option value="eiddle eastern">Middle Eastern</option>
+            <option value="nordic">Nordic</option>
+            <option value="southern">Southern</option>
+            <option value="spanish">Spanish</option>
+            <option value="thai">Thai</option>
+            <option value="vietnamese">Vietnamese</option>
+          </Form.Control>
+        </Form.Group>
+      </Form.Row>
+      <Form.Row>
+        <Form.Group as={Col} controlId="formGridText">
+          <Form.Label>Excluded ingredients</Form.Label>
+          <Form.Control
+            value={excludeIngredients}
+            onChange={e => setexcIudeIngredients(e.target.value)}
+            type="text"
+            placeholder="e.g Pasta"
+          />
+        </Form.Group>
+        <Form.Group as={Col} controlId="formGridState">
+          <Form.Label>Diet type</Form.Label>
+          <Form.Control
+            value={diet}
+            onChange={e => setDiet(e.target.value)}
+            as="select"
+          >
+            <option value="">All</option>
+            <option value="gluten free">Gluten Free</option>
+            <option value="vegetarian">Vegetarian</option>
+            <option value="lacto-vegetarian">Lacto-Vegetarian</option>
+            <option value="ovo-vegetarian">Ovo-Vegetarian</option>
+            <option value="vegan">Vegan</option>
+            <option value="pescetarian">Pescetarian</option>
+            <option value="paleo">Paleo</option>
+            <option value="primal">Primal</option>
+            <option value="whole30">Whole30</option>
+          </Form.Control>
+        </Form.Group>
+      </Form.Row>
+      <Form.Row>
+        <Form.Group as={Col} controlId="formGridText">
+          <Form.Label>How many recipes for you?</Form.Label>
+          <Form.Control
+            value={number}
+            onChange={e => setNumber(e.target.value)}
+            type="number"
+            placeholder="e.g 3- (up to 100)"
+          />
+        </Form.Group>
+        <Form.Group as={Col} controlId="formGridState">
+          <Form.Label>Intolerances</Form.Label>
+          <Form.Control
+            value={intolerances}
+            onChange={e => setIntolerances(e.target.value)}
+            as="select"
+          >
+            <option value="">None</option>
+            <option value="dairy">Dairy</option>
+            <option value="egg">Egg</option>
+            <option value="gluten">Gluten</option>
+            <option value="grain">Grain</option>
+            <option value="peanut">Peanut</option>
+            <option value="seafood">Seafood</option>
+            <option value="sesame">Sesame</option>
+            <option value="shellfish">Shellfish</option>
+            <option value="soy">Soy</option>
+            <option value="sulfite">Sulfite</option>
+            <option value="tree nut">Tree aNut</option>
+            <option value="wheat">Wheat</option>
+          </Form.Control>
+        </Form.Group>
+      </Form.Row>
+      <Button onClick={handleSubmit}>Submit</Button>
+    </Form>
+  );
+};
 
-export default SearchRecipe
+export default SearchRecipe;
